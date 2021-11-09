4ccb311ccbb090bcac405c89369fff5eb8557776
Mellgood <carlocentos@gmail.com>
2021-11-09 10:31:29 +0100

    ho aggiunto un 2 nel messaggio di hello

--- src/main/java/it/nttdata/hellospringboot/HellospringbootApplication.java
+++ src/main/java/it/nttdata/hellospringboot/HellospringbootApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HellospringbootApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Ciamo mondo");
+		System.out.println("Ciamo mondo 2");
 		SpringApplication.run(HellospringbootApplication.class, args);
 	}
 
